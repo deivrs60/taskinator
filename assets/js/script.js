1455eb9bfc9fc0236bed58d28a4b6ce4bf4d81aa
David Reyes Santoyo <deiv@Deivs-MBP.local>
2021-11-22 21:31:40 -0800

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-console.dir(window.document);
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
